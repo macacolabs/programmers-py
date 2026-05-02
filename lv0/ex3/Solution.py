9711c746d24372a050efa2191ae1c1d9fffb3061
macacolabs <monkey.ohgiraffers@gmail.com>
2026-05-02 19:32:33 +0900

    340205 clear

---
+++ lv0/ex3/Solution.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/340205
+# 2자리 이상의 정수 number가 주어집니다. 주어진 코드는 이 수를 2자리씩 자른 뒤, 자른 수를 모두
+# 더해서 그 합을 출력하는 코드입니다. 코드가 올바르게 작동하도록 한 줄을 수정해 주세요.
+
+number = int(input())
+
+answer = 0
+
+while number > 0:
+    answer += number % 100
+    number //= 100
+
+print(answer)
