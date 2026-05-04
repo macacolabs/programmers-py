ef6b55495d3ffdff2fb22d3923347f48c413bb12
macacolabs <monkey.ohgiraffers@gmail.com>
2026-05-04 12:34:25 +0900

    340203 clear

---
+++ lv0/ex5/Solution.py
@@ -0,0 +1,14 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/340203?language=python3
+
+def solution(cpr):
+    answer = []
+    basic_order = ["check", "call", "pressure", "respiration", "repeat"]
+    for action in cpr:
+        for i in range(len(basic_order)):
+            if action == basic_order[i]:
+                answer.append(i + 1)
+    return answer
+
+if __name__ == '__main__':
+    test_input = ["call", "respiration", "repeat", "check", "pressure"]
+    print(solution(test_input))
